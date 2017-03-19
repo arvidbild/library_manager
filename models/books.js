1a3eb2a7f98d189e99a3c02c7c05affa937b392e
Arvid <arvid.bild@studentvikarie.se>
2017-03-19 16:02:33 +0100

    project files

---
+++ models/books.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var books = sequelize.define('books', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    first_published: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return books;
+};
