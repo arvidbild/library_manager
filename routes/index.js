649673bf492536433610b7ca9b6b6bcd25af2e0a
Arvid <arvid.bild@studentvikarie.se>
2017-03-19 19:53:36 +0100

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("home");
 });
+
 
 module.exports = router;
