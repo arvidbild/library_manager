4206712afde6bfe7e0e4c3a5c13cdc07b7fe6c48
Arvid <arvid.bild@studentvikarie.se>
2017-03-20 08:31:57 +0100

    minor changes

--- routes/loans.js
+++ routes/loans.js
@@ -6,4 +6,20 @@ router.get("/all_loans",function (req,res,next) {
     res.render("all_loans");
 });
 
+/* GET /new_loan */
+router.get("/new_loan",function (req,res,next) {
+    res.render("new_loan");
+});
+
+/* GET /overdue_loans */
+router.get("/overdue_loans",function (req,res,next) {
+    res.render("overdue_loans");
+});
+
+/* GET /checked_loans */
+router.get("/checked_loans",function (req,res,next) {
+    res.render("checked_loans");
+});
+
+
 module.exports = router;
