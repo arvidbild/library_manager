6e549f4ec6e01fe5397a80b26fa2cecf61c3cd00
Arvid <arvid.bild@studentvikarie.se>
2017-03-20 08:31:31 +0100

    minor changes

--- routes/books.js
+++ routes/books.js
@@ -7,11 +7,24 @@ router.get("/all_books", function(req, res, next) {
   res.render("all_books");
 });
 
-
-/* GET /books/new. */
-router.get("/books/new", function(req, res, next) {
+/* GET /new_book */
+router.get("/new_book", function(req, res, next) {
   res.render("new_book");
 });
 
+/* GET /overdue_books */
+router.get("/overdue_books", function(req, res, next) {
+  res.render("overdue_books");
+});
+
+/* GET /checked_books */
+router.get("/checked_books", function(req, res, next) {
+  res.render("checked_books");
+});
+
+/* GET /return_book */
+router.get("/return_book", function(req, res, next) {
+  res.render("return_book");
+});
 
 module.exports = router;
